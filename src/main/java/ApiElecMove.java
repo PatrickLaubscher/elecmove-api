7a6a0d15eb8fe5a87e759e93ae533c379eace62d
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-05-19 18:46:25 +0200

    update mainclass

---
+++ src/main/java/ApiElecMove.java
@@ -0,0 +1,9 @@
+
+import org.springframework.boot.SpringApplication;
+
+
+public class ApiElecMove {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiElecMove.class, args);
+    }
+}
