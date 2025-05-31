cd0e7b3111cde0be6181850d83611a43f2b32f63
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-05-31 15:33:13 +0200

    Update ApiElecMove.java

--- src/main/java/fr/elecmove/api/ApiElecMove.java
+++ src/main/java/fr/elecmove/api/ApiElecMove.java
@@ -1,12 +1,10 @@
 package fr.elecmove.api;
 
-import fr.elecmove.api.config.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtConfig.class)
 public class ApiElecMove {
     public static void main(String[] args) {
         SpringApplication.run(ApiElecMove.class, args);
