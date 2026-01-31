286f3436d446dc322e9b758a4476a6c14f71a135
Patrick Laubscher <laubscherpatrick@gmail.com>
2026-01-31 09:18:30 +0100

    Add Lombok

--- src/main/java/fr/elecmove/api/controller/dto/review/ReviewCreationDTO.java
+++ src/main/java/fr/elecmove/api/controller/dto/review/ReviewCreationDTO.java
@@ -4,30 +4,17 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class ReviewCreationDTO {
 
     @NotNull
     @Size(min = 1, max = 300)
     private String comment;
+
     @NotNull
     @Min(0)
     @Max(5)
     private Integer rate;
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Integer getRate() {
-        return rate;
-    }
-
-    public void setRate(Integer rate) {
-        this.rate = rate;
-    }
 }
--- src/main/java/fr/elecmove/api/controller/dto/review/ReviewDTO.java
+++ src/main/java/fr/elecmove/api/controller/dto/review/ReviewDTO.java
@@ -1,63 +1,16 @@
 package fr.elecmove.api.controller.dto.review;
 
 import fr.elecmove.api.controller.dto.user.UserSingleDTO;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class ReviewDTO {
-
     private String id;
     private String comment;
     private Integer rate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private UserSingleDTO user;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Integer getRate() {
-        return rate;
-    }
-
-    public void setRate(Integer rate) {
-        this.rate = rate;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public UserSingleDTO getUser() {
-        return user;
-    }
-
-    public void setUser(UserSingleDTO user) {
-        this.user = user;
-    }
 }
